174e5a7792c2de00e3cf9fe45d055c369b8ca27f
Mr.R0b0t <150811866+Heitzlni@users.noreply.github.com>
2024-01-16 14:28:50 +0000

    Add todo input and display todos

--- ToDo/src/App.jsx
+++ ToDo/src/App.jsx
@@ -1,14 +1,32 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
 
+function App() {
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setTodos([...todos, todo]);
+
+  }
   return (
     <>
-      
-        
+
+      <input
+        type="text"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button onClick={handleSubmit} >Eingabe</button>
+      <div>
+        {todos}
+      </div>
     </>
   )
 }
 
+
 export default App
+
---
+++ test.js
@@ -0,0 +1,8 @@
+
+a = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+console.log(a[0])
+
+for (i = 0; i < a.length; i++) {
+    console.log(a[i])
+}
